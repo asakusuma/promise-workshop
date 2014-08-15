ed3283605f487e08420b4baf0fa92b93b9a1f5a5
asakusuma <asa.kusuma@gmail.com>
2014-08-15 12:10:29 -0700

    Clean up utility exercise

--- exercises/6-utility/answer.js
+++ exercises/6-utility/answer.js
@@ -1,10 +1,10 @@
 /**
- * Print out the array of promise messages. Experiment 
+ * Print out the array of promise messages. Experiment with map() and hash(). This is a freeform exercise.
  *
  * https://github.com/tildeio/rsvp.js/
  */
 
-function nameToMessage = function(item) {
+function nameToMessage(item) {
   return 'Hello my name is ' + item;
 };
 
@@ -12,5 +12,14 @@ var arrayOfMessagePromises = RSVP.map([
   api.getName1(),
   api.getName2(),
   api.getName3()
-], nameToMessage);
+], nameToMessage).then(_.partial(_.each, _, appendMessage));
 
+var hashOfMessagePromises = RSVP.hash({
+  chad: api.getName1(),
+  nate: api.getName2(),
+  asa: api.getName3()
+}).then(_.partial(api.mapHash, _, nameToMessage)).then(function(messages) {
+  appendMessage(messages.chad);
+  appendMessage(messages.nate);
+  appendMessage(messages.asa);
+});
--- exercises/6-utility/main.js
+++ exercises/6-utility/main.js
@@ -1,6 +1,5 @@
 /**
- * Print out the array of promise messages. Experiment with map() and hash().
- * Spread is currently not part of RSVP, so use api.spread()
+ * Print out the array of promise messages. Experiment with map() and hash(). This is a freeform exercise.
  *
  * https://github.com/tildeio/rsvp.js/
  */
@@ -13,14 +12,10 @@ var arrayOfMessagePromises = RSVP.map([
   api.getName1(),
   api.getName2(),
   api.getName3()
-], nameToMessage).then(_.partial(_.each, _, appendMessage));
+], nameToMessage);
 
 var hashOfMessagePromises = RSVP.hash({
   chad: api.getName1(),
   nate: api.getName2(),
   asa: api.getName3()
-}).then(_.partial(api.mapHash, _, nameToMessage)).then(function(messages) {
-  appendMessage(messages.chad);
-  appendMessage(messages.nate);
-  appendMessage(messages.asa);
 });
